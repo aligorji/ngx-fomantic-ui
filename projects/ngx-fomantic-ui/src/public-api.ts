cee43b4e3fd9c279a37fcc86f028c32af96dd998
Ali Gorji <aligorji68@gmail.com>
2023-11-16 22:37:49 +0330

    update to 14.3.0

--- projects/ngx-fomantic-ui/src/public-api.ts
+++ projects/ngx-fomantic-ui/src/public-api.ts
@@ -3,4 +3,13 @@ export * from './collections/public';
 export * from './modules/public';
 
 export * from './misc/public';
-export {FomanticUIModule} from './index';
+
+export * from './behaviors/internal';
+export * from './collections/internal';
+export * from './modules/internal';
+
+export * from './misc/internal';
+
+export { FomanticUIModule } from './fui.module';
+
+
